b26ea9100ce67e1fb6406a60db9570a3adc5d272
AbdullahAdnan05 <adnanabdulla54321@gmail.com>
2025-07-11 22:07:49 +0500

    for deployment changes

--- server.py
+++ server.py
@@ -12,5 +12,10 @@ register_callbacks(app)
 app.title = "Global Market Pulse"
 app.layout = get_layout()
 
-if __name__== "__main__":
-    app.run(debug=True)
+# if __name__== "__main__":
+#     app.run(debug=True)
+    
+if __name__ == "__main__":
+    from os import getenv
+    port = int(getenv("PORT", 8050))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
