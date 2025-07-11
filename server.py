c52524121b3e71c8142b81fc343000a632b9d897
AbdullahAdnan05 <adnanabdulla54321@gmail.com>
2025-07-11 23:10:50 +0500

    Add change to project

--- server.py
+++ server.py
@@ -1,21 +1,19 @@
 import dash
+from dash import Dash
 from dashboard.layout import get_layout
 from dashboard.callbacks import register_callbacks
 from dash_bootstrap_components.themes import CYBORG
 
-app = dash.Dash(__name__,
-                suppress_callback_exceptions=True,
-                external_stylesheets=[CYBORG]
-                )
+app = Dash(__name__,
+           suppress_callback_exceptions=True,
+           external_stylesheets=[CYBORG]
+)
 
 register_callbacks(app)
 app.title = "Global Market Pulse"
 app.layout = get_layout()
 
-# if __name__== "__main__":
-#     app.run(debug=True)
-    
 if __name__ == "__main__":
     from os import getenv
     port = int(getenv("PORT", 8050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run_server(debug=False, host="0.0.0.0", port=port)
